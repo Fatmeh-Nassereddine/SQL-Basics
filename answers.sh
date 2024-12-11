cdb2b0fc2796c0cd0d0e94d3b4cc180e11408d67
Fatmeh Nassereddine <Nassereddine.fatmeh@gmail.com>
2024-12-11 08:56:24 +0200

    basics querry

---
+++ answers.sh
@@ -0,0 +1,10 @@
+select * name from students
+select name FROM students
+select * FROM students WHERE age > 30
+select name FROM students WHERE age = 30 AND gender = 'female'
+select points FROM students WHERE name = 'Alex'
+
+update students SET points = points + 5 WHERE name = 'Basma'
+update students SET points = points - 2 WHERE name = 'Alex'
+
+
